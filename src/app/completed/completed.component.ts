e99ace2606b9f1f6a63901126876032ead7229e6
Bolarinwa Omoniyi <bolarinwaomoniyi@Bolarinwas-Air.fritz.box>
2023-10-26 14:40:31 +0200

    hi

--- src/app/completed/completed.component.ts
+++ src/app/completed/completed.component.ts
@@ -1,8 +1,9 @@
-import { Component, DoCheck } from '@angular/core';
+import { Component, DoCheck, Input } from '@angular/core';
 import { TodoModel } from '../todoModel';
 import { DatasourceService } from '../datasource.service';
 import { Observable, Subject, filter, map } from 'rxjs';
 import { UtilityService } from '../utility.service';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -12,14 +13,20 @@ import { UtilityService } from '../utility.service';
 })
 export class CompletedComponent implements DoCheck{
   
-  constructor(private httpService:DatasourceService, private utility:UtilityService){
+  constructor(private httpService:DatasourceService, private utility:UtilityService, private activatedRoute:ActivatedRoute){
+    this.activatedRoute.params.subscribe((data)=> 
+    this.completedPath = data["completed"])
   }
   ngDoCheck(): void {
+    this.httpService.getAllTodos().pipe(map((data)=> data.filter(todoModel => todoModel.isCompleted === true))).subscribe((data)=> (
+      this.todoReceiver = data
+    ))
     // console.log('About to publish',this.todoReceiver)
     // this.utility.publishCompletedTodo(this.todoReceiver)
   }
 
-
+  completedPath: string = ""
+  
   todoReceiver: TodoModel[] = [];
 
 
